d06dd70c971b35be2c4a7daaa2680dd421de837c
eto-pesets <root@server.pesets.tech>
2026-02-23 14:31:51 +0600

    move code to src/

--- core/round.js
+++
@@ -1,5 +0,0 @@
-export default function(value, precision) {
-    precision = precision || 1;
-	let n = Math.ceil(Math.log(precision) / Math.log(0.1));
-	return (Math.round(value / precision) * precision).toFixed(Math.max(0, n)) * 1;
-}
